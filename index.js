73375f39b2b7da87239a35505561a675e696cf65
gp622000 <Gyanprakash3434@gmail.com>
2021-02-10 00:23:27 +0530

    First commit: create the directory structure and setup express server

---
+++ index.js
@@ -0,0 +1,16 @@
+//calling the express
+const express = require('express');
+const app = express();
+const port = 8000;
+
+
+
+// `` is called backtic
+// this process is known as interpolation.
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error in running the server: ${err}`);
+    }
+    //console.log("Server is running at port: ",port) same as below;
+    console.log(`Server is running on port: ${port}`);
+});
