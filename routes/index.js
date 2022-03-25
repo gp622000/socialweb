88f58f89be5cb8459fb5baf4b28b888ed4808280
gp622000 <Gyanprakash3434@gmail.com>
2022-03-25 14:59:33 +0530

    api folders created and created some basic api's

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,19 @@
-const express = require('express');
+const express = require("express");
+const router = express.Router();
 
-const homeController = require('../controllers/home_controller');
-
+const homeController = require("../controllers/home_controller");
 
 // for diving the router we need this
-const router = express.Router();
 
 console.log("Router loaded");
 
-router.get('/',homeController.home);
-router.use('/users',require('./users'));
-router.use('/posts',require('./post'));
-router.use('/comments',require('./comments'));
+router.get("/", homeController.home);
+router.use("/users", require("./users"));
+router.use("/posts", require("./post"));
+router.use("/comments", require("./comments"));
 // for any further routes, acccess from here
 // router.use('/routerName',require('./routerfile'));
+
+router.use("/api", require("./api"));
 
 module.exports = router;
