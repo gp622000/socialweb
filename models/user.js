467bf2c65a00355d6e3aa0b92a60614a102435d9
gp622000 <Gyanprakash3434@gmail.com>
2022-03-25 12:33:33 +0530

    multer and diskstorage added

--- models/user.js
+++ models/user.js
@@ -1,23 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const multer = require("multer");
+const path = require("path");
+const AVATAR_PATH = path.join("/uploads/users/avatars");
 
-const userSchema = new mongoose.Schema({
-    email : {
-        type : String,
-        required : true,
-        unique : true
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password : {
-      type : String,
-      required : true  
+    password: {
+      type: String,
+      required: true,
     },
-    name : {
-        type : String,
-        required : true
-    }
-}, {
-    timestamps : true
+    name: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "..", AVATAR_PATH));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
 });
 
-const User = mongoose.model('User',userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
