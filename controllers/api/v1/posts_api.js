da1593cf58e53178e1cdf3d938d48ebe6980a757
gp622000 <Gyanprakash3434@gmail.com>
2022-03-27 20:14:06 +0530

    post delete api updated

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,46 @@
-module.exports.index = function (req, res) {
+const Post = require("../../../models/post");
+const Comment = require("../../../models/comment");
+
+module.exports.index = async function (req, res) {
+  let posts = await Post.find({})
+    .sort("-createdAt")
+    .populate("user")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "user",
+      },
+    });
+
   return res.json(200, {
     message: "List of posts",
-    posts: [],
+    posts: posts,
+  });
+};
+
+module.exports.destroy = function (req, res) {
+  Post.findById(req.params.id, function (err, post) {
+    // .id means converting tthe object id into string
+    // if (post.user == req.user.id) {
+    post.remove();
+    Comment.deleteMany({ post: req.params.id }, function (err) {
+      // if (req.xhr) {
+      //   return res.status(200).json({
+      //     data: {
+      //       post_id: req.params.id,
+      //     },
+      //     message: "Post deleted",
+      //   });
+      // }
+      // req.flash("success", "Post and associated comments deleted");
+      return res.json(200, {
+        message: "Post and associated comments deleted successfully",
+      });
+      // return res.redirect("back");
+    });
+    // } else {
+    //   req.flash("error", "You cannot delete this post");
+    //   return res.redirect("back");
+    // }
   });
 };
